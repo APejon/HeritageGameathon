21b0ef78b829a710df94b6832b1a033c8de645e7
Hassan Sarhan <hassanAsarhan@outlook.com>
2026-01-09 20:38:37 +0400

    Added animation to player movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,12 +4,13 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float animationSpeed = 1f;
     [SerializeField] private Sprite walkLeftSprite;
     [SerializeField] private Sprite walkRightSprite;
     [SerializeField] private Sprite walkIdleSprite;
     public SpriteRenderer CharacterSpriteRenderer;
     private Camera cam;
+    private Vector3 targetPosition;
 
 
     private void Awake()
@@ -20,33 +21,39 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        var dir = Vector3.zero;
+        Vector3 dir;
         if (Keyboard.current.dKey.wasPressedThisFrame)
         {
             dir = Vector3.right - Vector3.forward;
+            dir.Normalize();
             CharacterSpriteRenderer.sprite = walkRightSprite;
+            targetPosition = transform.position + dir * 1.75f;
         }
         else if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             dir = -Vector3.right + Vector3.forward;
+            dir.Normalize();
             CharacterSpriteRenderer.sprite = walkLeftSprite;
+            targetPosition = transform.position + dir * 1.75f;
         }
         else if (Keyboard.current.wKey.wasPressedThisFrame)
         {
             dir = Vector3.right + Vector3.forward;
+            dir.Normalize();
             CharacterSpriteRenderer.sprite = walkIdleSprite;
+            targetPosition = transform.position + dir * 1.75f;
         }
         else if (Keyboard.current.sKey.wasPressedThisFrame)
         {
             dir = -Vector3.right - Vector3.forward;
+            dir.Normalize();
             CharacterSpriteRenderer.sprite = walkIdleSprite;
+            targetPosition = transform.position + dir * 1.75f;
         }
         else
         {
             CharacterSpriteRenderer.sprite = walkIdleSprite;
         }
-        dir.Normalize();
-        dir.y = 0;
-        transform.position += dir * 1.75f;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, animationSpeed * Time.deltaTime);
     }
 }
