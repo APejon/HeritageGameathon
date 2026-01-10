5034095b5a1a72268bbf895ca087ca4b5be4f528
Hassan Sarhan <h.sarhan@mental.ae>
2026-01-10 22:21:50 +0400

    Sunspot

---
+++ Assets/Scripts/SunSpot.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SunSpot : MonoBehaviour
+{
+    [SerializeField] private ResourceBars resourceBars;
+    [SerializeField] private CinemachineCamera isometricCamera;
+    private float initialOrthographicSize;
+
+    private float time;
+
+
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+        initialOrthographicSize = Camera.main.orthographicSize;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DOTween.To(() => isometricCamera.Lens.OrthographicSize,
+                value => isometricCamera.Lens.OrthographicSize = value, 1.68f, 0.2f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            time = 0;
+            DOTween.To(() => isometricCamera.Lens.OrthographicSize,
+                value => isometricCamera.Lens.OrthographicSize = value, initialOrthographicSize, 0.2f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            time += Time.deltaTime;
+            if (time >= 1f)
+            {
+                time = 0;
+                resourceBars.decreaseResource(ResourceBars.stat.Hydration, 10);
+            }
+        }
+    }
+}
