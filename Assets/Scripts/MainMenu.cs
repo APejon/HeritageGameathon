f29e4ff8f87f02c046a5f8e44003f2b2ac1a087d
Ammar Albreiki <ammar.s.albreiki@gmail.com>
2026-01-11 12:10:35 +0400

    Main Menu Instructions added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button instructionButton;
+    [SerializeField] private RectTransform instructions;
+    [SerializeField] private RectTransform arrowImage;
+    private Boolean _hidden;
 
+    void Start()
+    {
+        _hidden = true;
+    }
 
     private void OnEnable()
     {
         playButton.onClick.AddListener(OnPlayButtonClicked);
         quitButton.onClick.AddListener(OnQuitButtonClicked);
+        instructionButton.onClick.AddListener(OnInstructionsClicked);
     }
 
     private void OnDisable()
@@ -33,5 +44,21 @@ public class MainMenu : MonoBehaviour
     {
         playButton.onClick.RemoveListener(OnPlayButtonClicked);
         SceneManager.LoadScene("Level1");
+    }
+
+    private void OnInstructionsClicked()
+    {
+        if (_hidden)
+        {
+            instructions.DOAnchorPos(new Vector3(-600, -360, 0), 0.5f);
+            arrowImage.DORotate(new Vector3(0, 0, 180), 0.5f);
+            _hidden = false;
+        }
+        else
+        {
+            instructions.DOAnchorPos(new Vector3(0, -360, 0), 0.5f);
+            arrowImage.DORotate(new Vector3(0, 0, 0), 0.5f);
+            _hidden = true;
+        }
     }
 }
