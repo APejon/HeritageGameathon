7b229b95a4879ef482c3fedc7da84d698bace507
Hassan Sarhan <h.sarhan@mental.ae>
2026-01-11 12:00:52 +0400

    Deleted debug log

--- Assets/Scripts/CharacterVisibilityToggler.cs
+++ Assets/Scripts/CharacterVisibilityToggler.cs
@@ -9,7 +9,6 @@ public class CharacterVisibilityToggler : MonoBehaviour
     public void ToggleVisibility()
     {
         var colliders = Physics.OverlapBox(transform.position, Vector3.one * 1.85f / 2f);
-        Debug.Log(colliders.Length);
         foreach (var collider in colliders)
         {
             var visibility = collider.gameObject.GetComponent<IVisibility>();
